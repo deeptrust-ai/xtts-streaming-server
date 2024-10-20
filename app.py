d83ce3d286d9b0bdab47e54d537def07bedcf115
Aman Ibrahim <amanmibra@gmail.com>
2024-10-20 14:39:06 -0700

    Move audio processing logic

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import io
 import os
+import tempfile
 import modal
-import modal.gpu
+import numpy as np
+import base64
+import wave
+from pydantic import BaseModel
+from typing import List
 
 app = modal.App(name="xtts")
 
@@ -13,17 +19,63 @@ image = (
 )
 
 with image.imports():
+    from trainer.io import get_user_data_dir
     from TTS.tts.configs.xtts_config import XttsConfig
     from TTS.tts.models.xtts import Xtts
-    from TTS.utils.generic_utils import get_user_data_dir
     from TTS.utils.manage import ModelManager
     import torch
-    # from TTS.api import TTS  
+
+class StreamingInputs(BaseModel):
+    speaker_embedding: List[float]
+    gpt_cond_latent: List[List[float]]
+    text: str
+    language: str
+    add_wav_header: bool = True
+    stream_chunk_size: str = "20"
+
+class TTSInputs(BaseModel):
+    speaker_embedding: List[float]
+    gpt_cond_latent: List[List[float]]
+    text: str
+    language: str
+
+
+def postprocess(wav):
+    """Post process the output waveform"""
+    if isinstance(wav, list):
+        wav = torch.cat(wav, dim=0)
+    wav = wav.clone().detach().cpu().numpy()
+    wav = wav[None, : int(wav.shape[0])]
+    wav = np.clip(wav, -1, 1)
+    wav = (wav * 32767).astype(np.int16)
+    return wav
+
+
+def encode_audio_common(
+    frame_input, encode_base64=True, sample_rate=24000, sample_width=2, channels=1
+):
+    """Return base64 encoded audio"""
+    wav_buf = io.BytesIO()
+    with wave.open(wav_buf, "wb") as vfout:
+        vfout.setnchannels(channels)
+        vfout.setsampwidth(sample_width)
+        vfout.setframerate(sample_rate)
+        vfout.writeframes(frame_input)
+
+    wav_buf.seek(0)
+    if encode_base64:
+        b64_encoded = base64.b64encode(wav_buf.getbuffer()).decode("utf-8")
+        return b64_encoded
+    else:
+        return wav_buf.read()
+
+
 
 @app.cls(
     image=image,
     gpu="A10G",
-    mounts=[modal.Mount.from_local_dir("server", remote_path="/root/server")]
+    # mounts=[modal.Mount.from_local_dir("server", remote_path="/root/server")],
+    keep_warm=1,
 )
 class XTTS:
     @modal.build()
@@ -67,6 +119,19 @@ class XTTS:
         import subprocess
 
         return subprocess.run(["nvcc", "--version"], check=True)
+
+    @modal.method()
+    def clone_speaker(self, audio: bytes):
+        temp_audio_name = next(tempfile._get_candidate_names())
+        with open(temp_audio_name, "wb") as temp, torch.inference_mode():
+            temp.write(io.BytesIO(audio).getbuffer())
+            gpt_cond_latent, speaker_embedding = self.model.get_conditioning_latents(
+                temp_audio_name
+            )
+        return {
+            "gpt_cond_latent": gpt_cond_latent.cpu().squeeze().half().tolist(),
+            "speaker_embedding": speaker_embedding.cpu().squeeze().half().tolist(),
+        }
     
     @modal.asgi_app()
     def web(self):
@@ -77,17 +142,34 @@ class XTTS:
         import io
         import tempfile
         import torch
-        import numpy as np
-        import base64
-        import wave
 
-        from server.main import (
-            postprocess,
-            encode_audio_common,
-            StreamingInputs,
-            TTSInputs,
-            predict_streaming_generator,
-        )
+
+        def predict_streaming_generator(parsed_input):
+            speaker_embedding = torch.tensor(parsed_input.speaker_embedding).unsqueeze(0).unsqueeze(-1)
+            gpt_cond_latent = torch.tensor(parsed_input.gpt_cond_latent).reshape((-1, 1024)).unsqueeze(0)
+            text = parsed_input.text
+            language = parsed_input.language
+
+            stream_chunk_size = int(parsed_input.stream_chunk_size)
+            add_wav_header = parsed_input.add_wav_header
+
+
+            chunks = self.model.inference_stream(
+                text,
+                language,
+                gpt_cond_latent,
+                speaker_embedding,
+                stream_chunk_size=stream_chunk_size,
+                enable_text_splitting=True,
+            )
+
+            for i, chunk in enumerate(chunks):
+                chunk = postprocess(chunk)
+                if i == 0 and add_wav_header:
+                    yield encode_audio_common(b"", encode_base64=False)
+                    yield chunk.tobytes()
+                else:
+                    yield chunk.tobytes()
 
         web_app = FastAPI(
             title="XTTS Streaming server",
